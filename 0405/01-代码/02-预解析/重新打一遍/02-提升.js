995d69380a347edf770b9921a593d310eb5f2f80
Jathon <hi@chenjia.xin>
2023-04-05 22:08:38 +0800

    Jathon commit

---
+++ 0405/01-代码/02-预解析/重新打一遍/02-提升.js
@@ -0,0 +1,20 @@
+function fn() {
+  console.log(1);
+}
+fn();
+function fn() {
+  console.log(2);
+}
+fn();
+function fn() {
+  console.log(3);
+}
+fn();
+var fn = function () {
+  console.log(4);
+};
+fn();
+function fn() {
+  console.log(5);
+}
+fn();
